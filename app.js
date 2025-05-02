e64ff974ccece716047b75d61b042ea753e76e33
StefPoss <7715423+StefPoss@users.noreply.github.com>
2025-05-02 10:58:42 +0200

    backend > affinage config serveur

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-require("dotenv").config()
+import dotenv from "dotenv"
+import express from "express"
+import path from "path"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
+import cors from "cors"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
 
-var express = require("express")
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
+import indexRouter from "./routes/index.js"
+import usersRouter from "./routes/users.js"
+import tweetsRouter from "./routes/tweets.js"
 
-var indexRouter = require("./routes/index")
-var usersRouter = require("./routes/users")
-var tweetsRouter = require("./routes/tweets")
+dotenv.config()
 
-var app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const cors = require("cors")
+const app = express()
+
 app.use(cors())
-
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(join(__dirname, "public")))
 
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
 app.use("/tweets", tweetsRouter)
 
-module.exports = app
+export default app
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from "express"
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.send("Route /tweets OK")
+  res.send("Bienvenue sur Hackatweet backend !")
 })
 
 export default router
